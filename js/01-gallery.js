fa68fb3e2f13fc5eeb9f8ca9aa82fb61c3f12f85
Kateryna Artemieva <moruga.katya@gmail.com>
2023-03-28 15:05:21 +0100

    start

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+let selectImgObj = {};
+
+const marcup = galleryItems
+  .map(({ preview, description, original }) => {
+    return `<li class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+  })
+  .join("");
+galleryList.insertAdjacentHTML("beforeend", marcup);
+
+galleryList.addEventListener("click", onGalleryElClick);
+
+function onGalleryElClick(evt) {
+  if (evt.target.classList.value !== "js-gallery-img") {
+    return;
+  }
+  const selectImgDescr = evt.target.alt;
+  selectImgObj = galleryItems.find(
+    (item) => item.description === selectImgDescr
+  );
+
+ const instance = basicLightbox.create(`
+ <div><img class="js-gallery-img" src="${selectImgObj.original}" alt="${selectImgObj.description}" width="1280"></div>
+ `);
+
+
+  instance.show();
+}
+
+
